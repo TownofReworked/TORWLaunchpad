05c2aad5055e440f3cdd335162be3dd82d5a8387
ang-xd <angxdxdd@gmail.com>
2025-02-18 21:40:07 -0500

    oops

--- LaunchpadReloaded/Options/Roles/Afterlife/Crewmate/TaskmasterOptions.cs
+++ LaunchpadReloaded/Options/Roles/Afterlife/Crewmate/TaskmasterOptions.cs
@@ -1,4 +1,5 @@
-﻿using MiraAPI.GameOptions;
+﻿using LaunchpadReloaded.Roles.Afterlife.Crewmate;
+using MiraAPI.GameOptions;
 using MiraAPI.GameOptions.Attributes;
 using MiraAPI.Utilities;
 using System;
@@ -9,7 +10,7 @@ public class TaskmasterOptions : AbstractOptionGroup
 {
     public override string GroupName => "Taskmaster";
 
-    public override Type AdvancedRole => typeof(TaskmasterOptions);
+    public override Type AdvancedRole => typeof(TaskmasterRole);
 
     [ModdedNumberOption("Steal Task Cooldown", 5, 120, 5, MiraNumberSuffixes.Seconds)]
     public float TaskmasterCooldown { get; set; } = 20;
